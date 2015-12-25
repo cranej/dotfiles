6a9bfb6a69d05d35b1a7881e65825507d42e3386
Crane Jin <crane@cranejin.com>
2015-12-25 17:24:20 +0800

    load devenv.exe to path by default

--- env.bat
+++ env.bat
@@ -1,5 +1,37 @@
 @echo off
 SET PATH=%USERPROFILE%\vimfiles\bin;%USERPROFILE%\bin;%PATH%
+
+IF NOT "%VS140COMNTOOLS%"=="" (
+    call :includevs14
+    GOTO :NEXT
+)
+
+IF NOT "%VS120COMNTOOLS%"=="" (
+    call :includevs12
+    GOTO :NEXT
+)
+
+IF NOT "%VS110COMNTOOLS%"=="" (
+    call :includevs11
+    GOTO :NEXT
+)
+
+:includevs14
+    SET VSPATH="%VS140COMNTOOLS:~0,-6%IDE\"
+    SET PATH=%VSPATH%;%PATH% 
+:end
+
+:includevs12
+    SET VSPATH="%VS120COMNTOOLS:~0,-6%IDE\"
+    SET PATH=%VSPATH%;%PATH% 
+:end
+
+:includevs11
+    SET VSPATH="%VS110COMNTOOLS:~0,-6%IDE\"
+    SET PATH=%VSPATH%;%PATH% 
+:end
+
+:NEXT
 SET MYPROMPT=[%COMPUTERNAME%] $p$S$+$_$g$S
 SET PROMPT=%MYPROMPT%
-TITLE I'm a shell ^^_^^
+TITLE I'm a shell ^^_^^     -    Extensions: vsshell, mbshell
