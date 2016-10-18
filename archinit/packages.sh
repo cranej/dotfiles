746258ae6e1d4126acc02d83b489c8b1ea4aa776
Crane Jin <crane@cranejin.com>
2016-10-18 21:16:52 +0800

    Add shortcut for passmenu.

--- archinit/packages.sh
+++ archinit/packages.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 pacman -Sy --noconfirm sudo git zsh xorg xorg-server xorg-server-utils xorg-xinit i3
-pacman -Sy --noconfirm rxvt-unicode dmenu firefox openssh adobe-source-han-sans-otc-fonts
+pacman -Sy --noconfirm scrot feh rxvt-unicode dmenu pass xdotool firefox openssh adobe-source-han-sans-otc-fonts
 pacman -Sy --noconfirm networkmanager network-manager-applet
 pacman -Sy --noconfirm transmission-gtk goldendict udiskie mutt offlineimap msmtp cantarell-fonts
 
