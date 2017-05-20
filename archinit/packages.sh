ef1d37fb707cbe3cd5f4bbf8b44a21955aa3eca2
Crane Jin <crane@cranejin.com>
2017-05-20 19:18:14 +0800

    Add pcmanfm

--- archinit/packages.sh
+++ archinit/packages.sh
@@ -4,4 +4,4 @@ pacman -Sy --noconfirm sudo git zsh xorg xorg-server xorg-server-utils xorg-xini
 pacman -Sy --noconfirm scrot feh rxvt-unicode dmenu pass xdotool firefox openssh adobe-source-han-sans-otc-fonts ttf-dejavu
 pacman -Sy --noconfirm acpi networkmanager network-manager-applet alsa-utils
 pacman -Sy --noconfirm fcitx fcitx-gtk2 fcitx-gtk3 fcitx-configtool fcitx-libpinyin
-pacman -Sy --noconfirm gvim transmission-gtk goldendict udiskie mutt offlineimap msmtp cantarell-fonts
+pacman -Sy --noconfirm gvim transmission-gtk goldendict udiskie mutt offlineimap msmtp cantarell-fonts pcmanfm gvfs-smb
