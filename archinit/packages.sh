a65a7f97fe2b93d93308f7064d1e20ae331914c5
Crane Jin <crane@cranejin.com>
2023-02-07 13:44:39 +0800

    Cleanup

--- archinit/packages.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-pacman -Sy --noconfirm sudo git zsh exfat-utils gnome
-pacman -Sy --noconfirm fcitx fcitx-gtk2 fcitx-gtk3 fcitx-configtool fcitx-libpinyin
-pacman -Sy --noconfirm rxvt-unicode dmenu pass xdotool firefox openssh adobe-source-han-sans-otc-fonts ttf-dejavu goldendict gvfs-smb
--- offlineimaprc.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import subprocess
-
-def get_gmail_password():
-    return subprocess.check_output(
-            ["pass email/crane.jin@outlook.com"], shell=True).strip('\n')
